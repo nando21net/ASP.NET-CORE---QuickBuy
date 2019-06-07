9bce917ead3290aac5b7a73e545ac93c859b3c38
Fernando Dias Seixas <webdesign_nando@hotmail.com>
2019-06-07 17:17:47 -0300

    Mapeamento das Entidades Relacionais

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QuickBuy.Repositorio.Contexto;
 
 namespace QuickBuy.Web
 {
@@ -12,7 +14,11 @@ namespace QuickBuy.Web
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("config.json", optional:false, reloadOnChange: true);
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -21,6 +27,13 @@ namespace QuickBuy.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //seLazyLoadingProxies() permite vcarregamento automatico das entidades relacionadas "Usuario/Pedido"
+            var connectionString = Configuration.GetConnectionString("QuickBuyDB");
+            services.AddDbContext<QuickBuyContexto>(option =>
+                                                              option.UseLazyLoadingProxies()
+                                                              .UseMySql(connectionString,
+                                                               m => m.MigrationsAssembly("QuickBuy.Repositorio")));
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
---
+++ QuickBuy/Config/ProdutoConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class ProdutoConfiguration : Microsoft.EntityFrameworkCore.IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            //Builder utiliza o padrão Fluent
+            builder
+                .Property(p => p.Nome)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder
+                .Property(u => u.Descricao)
+                .IsRequired()
+                .HasMaxLength(400);
+            builder
+                .Property(u => u.Preco)
+                .IsRequired();
+                
+        }
+    }
+}
